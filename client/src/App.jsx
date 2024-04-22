47fc2f6fa6183d85220487fb38e5f88c1e6a4e26
umtt31 <umutcan.adiguzel@outlook.com>
2024-04-22 03:16:06 +0300

    Responsive Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div>
+    <div className="App font-opensans">
       <HomePage />
     </div>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1565d8",
+        dark: {
+          hard: "#0D2436",
+          soft: "#183B56",
+        },
+      },
+      fontFamily: {
+        opensans: ["Open Sans", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
